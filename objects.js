319211f42b59c3a68acd9efbd7fd56f439dcc736
Daniel Brazel <dbrazel4@gmail.com>
2018-01-07 20:56:16 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {Stones: "Wild Horses"}
+
+function updatePlaylist(playlistName, artist, song){
+  return Object.assign({} ,playlistName, {[artist]: song})
+}
+
+function removeFromPlaylist(playlistName, artistName){
+  delete playlistName.artistName;
+  console.log(playlist);
+}
